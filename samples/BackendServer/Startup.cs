672f08d0f301caba13e03f4ed829117d0698fde8
GLOBAL\lsiddiquee <lsiddiquee@sdl.com>
2017-03-09 11:48:33 +0100

    adding a working sample

---
+++ samples/BackendServer/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.SqlServer;
+using Microsoft.Owin;
+using Owin;
+using Sdl.SignalR.Backplane.Common;
+using Sdl.SignalR.Backplane.Hub;
+using System;
+
+[assembly: OwinStartup(typeof(BackendServer.Startup))]
+
+namespace BackendServer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            string sqlConnectionString = "Data Source=(local);Initial Catalog=signalr;User ID=signalrsa;Password=Pass123";
+            Type messageBusType = typeof(SqlMessageBus);
+            Type messageBusConfigurationType = typeof(SqlScaleoutConfiguration);
+            Type hubType = typeof(SdlBackplaneHub);
+            HubLoader.Load(app, messageBusType.Assembly.FullName, messageBusConfigurationType.FullName, messageBusType.FullName, hubType.Assembly.FullName, true, sqlConnectionString);
+        }
+    }
+}
---
+++ samples/ChatSample/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+using Sdl.SignalR.SignalRMessageBus;
+
+[assembly: OwinStartup(typeof(ChatSample.Startup))]
+
+namespace ChatSample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalHost.DependencyResolver.UseSdlBackplane("http://localhost:61457/");
+            app.MapSignalR();
+        }
+    }
+}
