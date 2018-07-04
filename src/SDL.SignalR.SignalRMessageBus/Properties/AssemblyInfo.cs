673f1223a2d4f328e1b511cfe4140e3df987d274
ashatrov <ashatrov@sdl.com>
2018-07-04 08:44:13 +0300

    Updated nuget versions, updated csproj format.

--- src/SDL.SignalR.SignalRMessageBus/Properties/AssemblyInfo.cs
+++ src/SDL.SignalR.SignalRMessageBus/Properties/AssemblyInfo.cs
@@ -1,15 +1,6 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Sdl.SignalR.SignalRMessageBus")]
-[assembly: AssemblyDescription("A messaging backplane based on SignalR for scaling out of ASP.NET SignalR applications in a web-farm where the persistant storage will be handled by another backplane.")]
-[assembly: AssemblyCompany("SDL Group")]
-[assembly: AssemblyProduct("Sdl.SignalR.SignalRMessageBus")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,17 +11,4 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c57c403d-021b-452a-a3d7-06f72d3c1e90")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: InternalsVisibleTo("SDL.SignalR.SignalRMessageBus.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100fffe5635cbc37ad117f7bb3809f5d178d1383d0fd3969b3c07f19c2c1e424f12b6842e72dbdef4b984b923a675ef6b343b0492c6ca20f056225d1fcc20bd305e3560ca017fa021d667698217985ff994d8b16a76c4950941d40a24b8ec5f1e6a3e4c28d54cc51075f88aa78d2f5f307a73896324d7c5fe5edf94115269fb5a93")]
